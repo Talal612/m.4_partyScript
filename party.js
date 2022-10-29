cb90bc95f3a5e4e1e02ea9d94853b5aa7465cf26
Talal_Otaibi <77454679+Talal612@users.noreply.github.com>
2022-10-29 15:10:46 +0300

    Update party.js

--- party.js
+++ party.js
@@ -1,22 +1,19 @@
-const minAge = 18;
-const maxAge = 35;
-    do {  
-      age=prompt('Hi there,Please Enter your age:');
-      if(age>=minAge && age<=maxAge)
-      {
-        namee=prompt('Please Enter your name:');
-        prompt('Welcome ',namee ,'!');
-      }
-      
-      else if(age<minAge) 
-      { 
-        prompt('you are too young');
-      }
+let UserAge;
+    
 
-      else if(age>maxAge)
-      {
-        prompt('you are too old');
-      }
+do {  
+      UserAge=prompt('Hi there,Please Enter your age:');
+      
+      if(UserAge<18) 
+        alert('you are too young');
+     
+
+      else if(UserAge>35)
+        alert('you are too old !');
+   
+      else
+        alert('Hi '+prompt('Please Enter your name:') +' , Welcome to the party!');
+      
 
     }while("Is there anyone else?")
 
